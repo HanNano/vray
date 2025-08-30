614e239f651d4a0034308eabcf4ba56af3d713ce
HanLM <139763054+HanNano@users.noreply.github.com>
2025-08-31 00:14:42 +0630

    Update xpass.js

--- xpass.js
+++ xpass.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'c24bcc00-0548-5518-ff7f-20cbcd8bebcd';
+let userID = 'fefeb8f0-768d-4a26-8523-48ef184fd00b';
 
 const พร็อกซีไอพีs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
 
